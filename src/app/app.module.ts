30b41f12eead24875b9d9da596e028dc6a6c4dbd
niw22 <ira.watt@newcastle.ac.uk>
2019-06-10 09:57:26 +0100

    added the graph

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ChartsModule } from 'ng2-charts';
 //import { MaterialModule } from './material.module';
 import { AngularFullpageModule } from '@fullpage/angular-fullpage';
+import { PiechartComponent } from './piechart/piechart.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PiechartComponent
   ],
   imports: [
+  ChartsModule,
     BrowserModule,
    // MaterialModule,
     AppRoutingModule,
