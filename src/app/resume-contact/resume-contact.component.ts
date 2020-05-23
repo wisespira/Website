9933c44bc7d390bde89bf92599e16af0e3bd1256
Ira Watt <irawatt@gmail.com>
2020-05-23 23:14:46 +0100

    Use IFrame on IE and Edge

--- src/app/resume-contact/resume-contact.component.ts
+++ src/app/resume-contact/resume-contact.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DeviceDetectorService } from 'ngx-device-detector';
 @Component({
   selector: 'app-resume-contact',
   templateUrl: './resume-contact.component.html',
@@ -7,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResumeContactComponent implements OnInit {
  pdfSrc: string = 'assets/Ira Watt Resume.pdf';
-  constructor() { }
+  constructor(private deviceService: DeviceDetectorService) {
+    console.log(this.deviceService)
+    if(this.deviceService.browser == "MS-Edge"){
+       this.EdgebrowserCheck = false;}
+       if(this.deviceInfo.browser == "IE"){
+          this.IEbrowserCheck = false;}
+
+
+      }
+
+EdgebrowserCheck = true;
+   IEbrowserCheck = true;
 
   ngOnInit() {
   }
