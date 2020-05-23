b8dbcb465201b34ae46701a7a13a67e6da13fcb4
Ira Watt <irawatt@gmail.com>
2020-05-23 18:00:25 +0100

    hide pages on IE

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DeviceDetectorService } from 'ngx-device-detector';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private deviceService: DeviceDetectorService) {
+console.log(this.deviceService)
+if(this.deviceService.browser == "IE"){
+   this.IEbrowserCheck = false;}
+  }
+IEbrowserCheck = true;
   ngOnInit() {
   }
 
